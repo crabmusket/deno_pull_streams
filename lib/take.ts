e8cb8253c4af8418187514bbec395316451ef1a2
Daniel Buckmaster <daniel@crabmusket.net>
2019-08-20 09:56:15 +1000

    Validate the input to take

--- lib/take.ts
+++ lib/take.ts
@@ -1,6 +1,16 @@
 import { Cont, End, Source, Through } from "./types.ts";
 
 export function take<V>(n: number): Through<V, V> {
+  if (isNaN(n)) {
+    throw new Error("cannot take NaN elements");
+  }
+  if (n < 0) {
+    throw new Error("cannot take a negative number of elements");
+  }
+  if (!isFinite(n)) {
+    throw new Error("cannot take infinitely many elements, that is pointless");
+  }
+
   let remaining = n;
   return function takeThrough(read: Source<V>): Source<V> {
     return function takeSource(end: End, cont: Cont<V>) {
